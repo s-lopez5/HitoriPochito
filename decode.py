791b417fedcb49ea72766bbbbe3d663fcf0a1a05
s-lopez5 <s.lopez5@udc.es>
2022-03-09 00:33:18 +0100

    Primer intento

---
+++ decode.py
@@ -0,0 +1,13 @@
+import pygame
+import clingo
+import sys
+
+def read(): 
+    
+    
+    data = open("out.txt")
+    l = data.read()
+    
+    print(l)
+    
+read()
---
+++ encode.py
@@ -0,0 +1,36 @@
+import pygame
+import clingo
+import sys
+
+def to_list(table):
+    return list(table)
+
+def leer ():
+    a = open(sys.argv[1])
+    b = a.read()
+    c = to_list(b)
+
+    num = 0
+    i = 0
+    j = 0
+    string = ""
+
+    for x in c:
+        if x == "\n":
+            string += "\n"
+            num = i
+            i = 0
+            j += 1
+        else: 
+            string += "cell(" + str(i) + "," + str(j) + "," + x + ")."
+            i += 1
+    string2 = "digit(1.." + str(num) + ").\nrow(0.." + str(num-1) + ").\ncolumn(0.. " + str(num-1) + ").\n"
+    string2 += string
+    s = open(sys.argv[1].replace("txt","lp"), "w+")
+    s.write(string2)
+    a.close
+    s.close
+
+leer()
+
+
